a10310055e3ead9ae7864255f1578cb7f2fdb772
Bruno Cesar <bruno_cco@hotmail.com>
2022-08-24 23:03:57 -0300

    Adicionando Gettrs e Settrs

--- proj-curso-java/src/cursojava/classes/Aluno.java
+++ proj-curso-java/src/cursojava/classes/Aluno.java
@@ -11,6 +11,53 @@ public class Aluno {
 	private	String nomeMae;
 	private	String nomePai;
 	private	String nomeEscola;
+	private String dataMatricula;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	
+	
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
 	private	String serieMatriculado;
 	
 	public Aluno() {/*Cria os dados na memória sendo padrão do Java*/
@@ -25,10 +72,107 @@ public class Aluno {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(String dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	/* Método que retorna a média do aluno*/
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+	
+	/* Método que retorna true para aprovado e false para reprovado*/
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if(media >= 70) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	/*Método que retorna string com frase de aprovado / reprovado*/
+	public String getAlunoAprovado2() {
+		double media = this.getMediaNota();
+		if(media >= 70) {
+			return "Aluno está aprovado";
+		}else {
+			return "Aluno está reprovado";
+		}
+	}
 	
 	/* veremos os métodos Getters e Setters do Objeto*/
 	/* SET é para adicionar ou receber dados para os atributos*/
-	/* */
+	/* GET é para resgatar ou obter o valor do atributo*/
 	
 	
 
--- proj-curso-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ proj-curso-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -10,6 +10,28 @@ public class PrimeiraClasseJava {
 		/*new Aluno() é uma instância ( criação de objeto)*/
 		/*aluno1 é uma referência para o objeto aluno*/
 		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Bruno Cesar");
+		aluno1.setIdade(35);
+		aluno1.setDataDeNascimento("18/12/1986");
+		aluno1.setRegistroGeral("1111.1111.11");
+		aluno1.setNumeroCpf("999.999.999-99");
+		aluno1.setNomeMae("Maria");
+		aluno1.setNomePai("João");
+		aluno1.setDataMatricula("01/01/2019");
+		aluno1.setSerieMatriculado("5");
+		aluno1.setNomeEscola("Escola Dev Java");
+		aluno1.setNota1(90);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(90.7);
+		
+		
+		System.out.println("O Nome é: " + aluno1.getNome());
+		System.out.println("A idade é: " + aluno1.getIdade());
+		System.out.println("A data de nascimento é: " + aluno1.getDataDeNascimento());
+		System.out.println("A Média da nota é: " + aluno1.getMediaNota());
+		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
 
 	}
 
